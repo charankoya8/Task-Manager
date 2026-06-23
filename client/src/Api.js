d9eb9ed90b50a54e8e36f406269611ad4af697ea
Charan <charan_koyasrmap.edu.in@gmail.com>
2026-06-23 23:57:38 +0530

    Connect frontend to deployed backend

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api/tasks",
+  baseURL: "https://task-manager-backend-jrwg.onrender.com/api/tasks",
 });
 
 export default API;
